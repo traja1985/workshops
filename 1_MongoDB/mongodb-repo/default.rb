46100261ba5e24fe51e7d6a5b31f95e40191e142
raja_t@utexas.edu <raja_t@utexas.edu>
2018-03-05 12:39:27 -0500

    adding mongodb-repo 1. configures yum.repos.d 2. installs mongodb 3. starts mongodb service

---
+++ 1_MongoDB/mongodb-repo/default.rb
@@ -0,0 +1,21 @@
+file '/etc/yum.repos.d/mongodb-org-3.6.repo' do
+  content '[mongodb-org-3.6]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.6/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
+'
+#  source 'out.repo.erb'
+end#
+
+package 'mongodb-org'
+
+service 'mongod' do
+  action [:enable, :start]
+end
+
+# Cookbook:: mongodb
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
