702197da6d53dbdc699a0fd79d64a7e69ebbeb69
raja_t@utexas.edu <raja_t@utexas.edu>
2018-03-05 17:22:21 -0500

    Adding inspec test to the tomcat example

--- 2_Tomcat/cookbooks/learn_chef_cookbook/README.md
+++ 2_Tomcat/cookbooks/learn_chef_cookbook/README.md
@@ -3,3 +3,30 @@
 TO RUN :  sudo chef-client --local-mode --runlist 'recipe[learn_chef_cookbook]'
 
 
+To test using inspec
+
+RUN : inspec exec test/smoke/default/default_test.rb
+
+# Sample Output :
+Profile: tests from test/smoke/default/default_test.rb
+Version: (not specified)
+Target:  local://
+
+
+  User root
+     ↺
+  Port 80
+     ↺
+  System Package
+     ✔  java-1.7.0-openjdk-devel should be installed
+  Group tomcat
+     ✔  should exist
+  User tomcat
+     ✔  should exist
+  File /opt/tomcat
+     ✔  should be directory
+     ✔  should be owned by "root"
+
+Test Summary: 5 successful, 0 failures, 2 skipped
+
+
--- 2_Tomcat/cookbooks/learn_chef_cookbook/test/smoke/default/default_test.rb
+++ 2_Tomcat/cookbooks/learn_chef_cookbook/test/smoke/default/default_test.rb
@@ -1,12 +1,13 @@
-# # encoding: utf-8
+# encoding: utf-8
 
 # Inspec test for recipe learn_chef_cookbook::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+# for more info on resprce types go to http://serverspec.org/resource_types.html
 unless os.windows?
-  # This is an example test, replace with your own test.
+# This is an example test, replace with your own test.
   describe user('root'), :skip do
     it { should exist }
   end
@@ -15,4 +16,20 @@ end
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
+end
+
+describe package ('java-1.7.0-openjdk-devel') do
+  it { should be_installed }
+end 
+
+describe group ('tomcat') do
+  it { should exist }
+end 
+describe user ('tomcat') do
+  it { should exist }
+end
+
+describe file('/opt/tomcat') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
 end
